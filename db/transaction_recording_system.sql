655826e6848eb475f48263a61f27f9b77f493c25
Kenneth <neth3039@gmail.com>
2025-12-12 07:17:22 +0800

    updated sql file

--- db/transaction_recording_system.sql
+++ db/transaction_recording_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 06, 2025 at 08:37 AM
+-- Generation Time: Dec 12, 2025 at 12:16 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -30,14 +30,28 @@ SET time_zone = "+00:00";
 CREATE TABLE `audit_logs` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `user_id` int(10) UNSIGNED NOT NULL,
-  `branch_id` int(10) UNSIGNED NOT NULL,
+  `branch_id` int(10) UNSIGNED DEFAULT NULL,
   `transaction_id` bigint(20) UNSIGNED DEFAULT NULL,
   `description` varchar(500) DEFAULT NULL,
-  `action_type` enum('edit','delete','void','create','adjustment') NOT NULL,
+  `action_type` varchar(55) DEFAULT NULL,
   `old_data` longtext DEFAULT NULL,
   `new_data` longtext DEFAULT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `audit_logs`
+--
+
+INSERT INTO `audit_logs` (`id`, `user_id`, `branch_id`, `transaction_id`, `description`, `action_type`, `old_data`, `new_data`, `created_at`) VALUES
+(128, 1, 1, NULL, 'User login successful', 'LOGIN', NULL, NULL, '2025-12-11 23:03:21'),
+(129, 1, 1, NULL, 'User logged in', 'login', NULL, NULL, '2025-12-11 23:05:02'),
+(130, 2, 0, NULL, 'User logged in', 'login', NULL, NULL, '2025-12-11 23:05:11'),
+(131, 31, 1, NULL, 'User logged in', 'login', NULL, NULL, '2025-12-11 23:05:32'),
+(132, 2, 0, NULL, 'User logged in', 'login', NULL, NULL, '2025-12-11 23:05:50'),
+(133, 2, 0, NULL, 'User logged in', 'login', NULL, NULL, '2025-12-11 23:20:59'),
+(134, 31, 1, NULL, 'User logged in', 'login', NULL, NULL, '2025-12-11 23:21:12'),
+(135, 2, 0, NULL, 'User logged in', 'login', NULL, NULL, '2025-12-11 23:21:31');
 
 -- --------------------------------------------------------
 
@@ -54,6 +68,13 @@ CREATE TABLE `branches` (
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `branches`
+--
+
+INSERT INTO `branches` (`id`, `branch_name`, `address`, `current_coh`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'MAIN BRANCH', 'CALBAYOG', 1000.00, 1, '2025-12-09 03:33:49', '2025-12-10 21:08:47');
 
 -- --------------------------------------------------------
 
@@ -125,15 +146,25 @@ CREATE TABLE `transactions` (
 
 CREATE TABLE `users` (
   `id` int(10) UNSIGNED NOT NULL,
-  `branch_id` int(10) UNSIGNED NOT NULL,
+  `branch_id` int(10) UNSIGNED DEFAULT NULL,
   `name` varchar(255) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `role` enum('admin','cashier','viewer') NOT NULL DEFAULT 'cashier',
+  `role` enum('super_admin','admin','cashier') NOT NULL DEFAULT 'cashier',
   `status` tinyint(1) NOT NULL DEFAULT 1,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `branch_id`, `name`, `username`, `password`, `role`, `status`, `created_at`, `updated_at`) VALUES
+(1, 1, 'test test', 'admin', '$2y$10$zv5Z8i1r1IF.4/D7RrVC4uBnBXnbTx3yFihjAxPtHa07CHbXboJiW', 'admin', 1, '2025-12-09 03:35:57', '2025-12-11 22:18:07'),
+(2, 0, 'super admin', 'super_admin', '$2y$10$B8DHSKdCEG0kDzSGwNz.Re8vUWGOhLg5jwHGnYK5QcBBx/sqgnHIS', 'super_admin', 1, '2025-12-09 04:47:16', '2025-12-10 21:12:07'),
+(31, 1, 'cashier', 'cashier', '$2y$10$AaOj4VTtJ9Cafen/RkOpP.mXo2jk7MRQ8QU6awi09dGf9WTDKgiUW', 'cashier', 1, '2025-12-11 22:02:39', '2025-12-11 23:21:03'),
+(32, 1, 'kenken', 'kenken123', '$2y$10$fEU0CQXsph1PP919CuWaIeHpz1Vr0jNiYtXqVM1l2De.fQ.yCUiZ2', 'admin', 0, '2025-12-11 23:22:05', '2025-12-11 23:22:47');
 
 -- --------------------------------------------------------
 
@@ -176,8 +207,6 @@ CREATE TABLE `wallet_providers` (
 --
 ALTER TABLE `audit_logs`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_audit_transaction` (`transaction_id`),
-  ADD KEY `idx_audit_branch` (`branch_id`),
   ADD KEY `idx_audit_user` (`user_id`);
 
 --
@@ -219,8 +248,7 @@ ALTER TABLE `transactions`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD KEY `fk_users_branch` (`branch_id`);
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- Indexes for table `wallet_accounts`
@@ -245,13 +273,13 @@ ALTER TABLE `wallet_providers`
 -- AUTO_INCREMENT for table `audit_logs`
 --
 ALTER TABLE `audit_logs`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=136;
 
 --
 -- AUTO_INCREMENT for table `branches`
 --
 ALTER TABLE `branches`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `coh_logs`
@@ -275,7 +303,7 @@ ALTER TABLE `transactions`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT for table `wallet_accounts`
@@ -297,8 +325,6 @@ ALTER TABLE `wallet_providers`
 -- Constraints for table `audit_logs`
 --
 ALTER TABLE `audit_logs`
-  ADD CONSTRAINT `fk_audit_branch` FOREIGN KEY (`branch_id`) REFERENCES `branches` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_audit_transaction` FOREIGN KEY (`transaction_id`) REFERENCES `transactions` (`id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_audit_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE;
 
 --
