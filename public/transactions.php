714b5f576f5f2d4e731494877093f051220e9b9f
Kenneth <neth3039@gmail.com>
2026-01-08 22:27:30 +0800

    added session checker in transactions public

--- public/transactions.php
+++ public/transactions.php
@@ -4,7 +4,7 @@ require '../config/session_checker.php';
 <!DOCTYPE html>
 <html lang="en">
 <?php include '../views/head.php'; ?>
-
+<?php include '../processes/session_checker.php'; ?>
 <body>
     <?php include '../views/sidebar.php'; ?>
 
